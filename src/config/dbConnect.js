7d56aa838e85810629cd5adc88f98f8e0f0dd7ba
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-11-06 18:18:02 -0300

    Delete src directory

--- src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb+srv://IzabelaFranca:123@crud-livraria.ygfn6uy.mongodb.net/livraria")
-
-let db = mongoose.connection;
-
-export default db;
